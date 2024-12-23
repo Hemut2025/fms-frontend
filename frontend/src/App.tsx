db31aa5af1f54e561a984fd2fb3485a7ec920891
anthonyguerrera <anguerrera1@gmail.com>
2024-12-22 20:16:18 -0800

    initial env

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import StartText from './screen1.tsx';
+
+function App() {
+  return <div ><StartText /></div>;
+}
+
+export default App;
---
+++ frontend/src/screen1.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function StartText() {
+    return <h1>starting text</h1>;
+}
+
+export default StartText;
