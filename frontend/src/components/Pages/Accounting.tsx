20c5c7b07fcd405b3421c684198cb6b1db05d3e5
Mert Turna <mertturna@MacBook-Pro-Mert.local>
2024-12-24 18:31:55 -0800

    mert's commit

--- frontend/src/components/Pages/Accounting.tsx
+++ frontend/src/components/Pages/Accounting.tsx
@@ -2,7 +2,7 @@ const Accounting = () => {
   return (
     <div className="page-content">
       <h2>Accounting</h2>
-      <p>Welcome to the Accounting page. This is a test content.</p>
+      <p>Welcome to the Accounting page. This is a test content from Mert.</p>
     </div>
   );
 };
